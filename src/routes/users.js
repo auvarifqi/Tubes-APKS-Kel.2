cddcc4f6a8a41670aa6006d5f5dd297070b1c581
RakaAF123 <rakaadmiharfan@gmail.com>
2023-11-28 19:36:00 +0700

    set up docker postman

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const upload = require('../utils/multer');
-const User = require('../models/user');
-const auth = require('../middlewares/auth');
+const express = require("express");
+const upload = require("../utils/multer");
+const User = require("../models/user");
+const auth = require("../middlewares/auth");
 
 const router = new express.Router();
 
 // Create a user
-router.post('/users', async (req, res) => {
+router.post("/users", async (req, res) => {
   try {
-    const {role} = req.body;
-    if (role) throw new Error('you cannot set role property.');
+    //const { role } = req.body;
+    // if (role) throw new Error('you cannot set role property.');
     const user = new User(req.body);
     await user.save();
     const token = await user.generateAuthToken();
@@ -19,49 +19,55 @@ router.post('/users', async (req, res) => {
   }
 });
 
-router.post('/users/photo/:id', upload('users').single('file'), async (req, res, next) => {
-  const url = `${req.protocol}://${req.get('host')}`;
-  const { file } = req;
-  const userId = req.params.id;
-  try {
-    if (!file) {
-      const error = new Error('Please upload a file');
-      error.httpStatusCode = 400;
-      return next(error);
+router.post(
+  "/users/photo/:id",
+  upload("users").single("file"),
+  async (req, res, next) => {
+    const url = `${req.protocol}://${req.get("host")}`;
+    const { file } = req;
+    const userId = req.params.id;
+    try {
+      if (!file) {
+        const error = new Error("Please upload a file");
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+      const user = await User.findById(userId);
+      if (!user) return res.sendStatus(404);
+      user.imageurl = `${url}/${file.path}`;
+      await user.save();
+      res.send({ user, file });
+    } catch (e) {
+      res.sendStatus(400).send(e);
     }
-    const user = await User.findById(userId);
-    if (!user) return res.sendStatus(404);
-    user.imageurl = `${url}/${file.path}`;
-    await user.save();
-    res.send({ user, file });
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(400).send(e);
   }
-});
+);
 
 // Login User
-router.post('/users/login', async (req, res) => {
+router.post("/users/login", async (req, res) => {
   try {
-    const user = await User.findByCredentials(req.body.username, req.body.password);
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
     const token = await user.generateAuthToken();
     res.send({ user, token });
   } catch (e) {
     res.status(400).send({
-      error: { message: 'You have entered an invalid username or password' },
+      error: { message: "You have entered an invalid username or password" },
     });
   }
 });
 
-router.post('/users/login/facebook', async (req, res) => {
+router.post("/users/login/facebook", async (req, res) => {
   const { email, userID, name } = req.body;
-  const nameArray = name.split(' ');
+  const nameArray = name.split(" ");
 
   const user = await User.findOne({ facebook: userID });
   if (!user) {
     const newUser = new User({
       name,
-      username: nameArray.join('') + userID,
+      username: nameArray.join("") + userID,
       email,
       facebook: userID,
     });
@@ -78,15 +84,15 @@ router.post('/users/login/facebook', async (req, res) => {
   }
 });
 
-router.post('/users/login/google', async (req, res) => {
+router.post("/users/login/google", async (req, res) => {
   const { email, googleId, name } = req.body;
-  const nameArray = name.split(' ');
+  const nameArray = name.split(" ");
 
   const user = await User.findOne({ google: googleId });
   if (!user) {
     const newUser = new User({
       name,
-      username: nameArray.join('') + googleId,
+      username: nameArray.join("") + googleId,
       email,
       google: googleId,
     });
@@ -104,7 +110,7 @@ router.post('/users/login/google', async (req, res) => {
 });
 
 // Logout user
-router.post('/users/logout', auth.simple, async (req, res) => {
+router.post("/users/logout", auth.simple, async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter((token) => {
       return token.token !== req.token;
@@ -117,7 +123,7 @@ router.post('/users/logout', auth.simple, async (req, res) => {
 });
 
 // Logout all
-router.post('/users/logoutAll', auth.enhance, async (req, res) => {
+router.post("/users/logoutAll", auth.enhance, async (req, res) => {
   try {
     req.user.tokens = [];
     await req.user.save();
@@ -128,10 +134,10 @@ router.post('/users/logoutAll', auth.enhance, async (req, res) => {
 });
 
 // Get all users
-router.get('/users', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
+router.get("/users", auth.enhance, async (req, res) => {
+  if (req.user.role !== "superadmin")
     return res.status(400).send({
-      error: 'Only the god can see all the users!',
+      error: "Only the god can see all the users!",
     });
   try {
     const users = await User.find({});
@@ -142,7 +148,7 @@ router.get('/users', auth.enhance, async (req, res) => {
 });
 
 // User infos
-router.get('/users/me', auth.simple, async (req, res) => {
+router.get("/users/me", auth.simple, async (req, res) => {
   try {
     res.send(req.user);
   } catch (e) {
@@ -151,10 +157,10 @@ router.get('/users/me', auth.simple, async (req, res) => {
 });
 
 // Get user by id only for admin
-router.get('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
+router.get("/users/:id", auth.enhance, async (req, res) => {
+  if (req.user.role !== "superadmin")
     return res.status(400).send({
-      error: 'Only the god can see the user!',
+      error: "Only the god can see the user!",
     });
   const _id = req.params.id;
   try {
@@ -167,12 +173,14 @@ router.get('/users/:id', auth.enhance, async (req, res) => {
 });
 
 // Edit/Update user
-router.patch('/users/me', auth.simple, async (req, res) => {
-  console.log(req.body);
+router.patch("/users/me", auth.simple, async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'phone', 'username', 'email', 'password'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-  if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' });
+  const allowedUpdates = ["name", "phone", "username", "email", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).send({ error: "Invalid updates!" });
 
   try {
     const { user } = req;
@@ -185,18 +193,28 @@ router.patch('/users/me', auth.simple, async (req, res) => {
 });
 
 // Admin can update user by id
-router.patch('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
+router.patch("/users/:id", auth.enhance, async (req, res) => {
+  if (req.user.role !== "superadmin")
     return res.status(400).send({
-      error: 'Only the god can update the user!',
+      error: "Only the god can update the user!",
     });
   const _id = req.params.id;
 
   const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'phone', 'username', 'email', 'password', 'role'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+  const allowedUpdates = [
+    "name",
+    "phone",
+    "username",
+    "email",
+    "password",
+    "role",
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-  if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' });
+  if (!isValidOperation)
+    return res.status(400).send({ error: "Invalid updates!" });
 
   try {
     const user = await User.findById(_id);
@@ -211,10 +229,10 @@ router.patch('/users/:id', auth.enhance, async (req, res) => {
 });
 
 // Delete by id
-router.delete('/users/:id', auth.enhance, async (req, res) => {
-  if (req.user.role !== 'superadmin')
+router.delete("/users/:id", auth.enhance, async (req, res) => {
+  if (req.user.role !== "superadmin")
     return res.status(400).send({
-      error: 'Only the god can delete the user!',
+      error: "Only the god can delete the user!",
     });
   const _id = req.params.id;
 
@@ -222,16 +240,27 @@ router.delete('/users/:id', auth.enhance, async (req, res) => {
     const user = await User.findByIdAndDelete(_id);
     if (!user) return res.sendStatus(404);
 
-    res.send({ message: 'User Deleted' });
+    res.send({ message: "User Deleted" });
   } catch (e) {
     res.sendStatus(400);
   }
 });
 
-router.delete('/users/me', auth.simple, async (req, res) => {
-  if (req.user.role !== 'superadmin')
+router.delete("/users/testDelete/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    res.send({ message: "User Deleted" });
+  } catch (e) {
+    res.sendStatus(400);
+  }
+});
+
+router.delete("/users/me", auth.simple, async (req, res) => {
+  if (req.user.role !== "superadmin")
     return res.status(400).send({
-      error: 'You cannot delete yourself!',
+      error: "You cannot delete yourself!",
     });
   try {
     await req.user.remove();
