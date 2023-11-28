1eecbe524af763cfc2689fa76c0949753a5fdcbf
amjadadhie <prasetyoamjad@gmail.com>
2023-11-28 23:13:12 +0700

    update monitoring

---
+++ src/routes/prom.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const  router = new express.Router();
+const client = require ("prom-client");
+
+router -get("/metrics", async (req, res) =>{
+    res.set ("Content-Type", client.register.contentType);
+    return res.send(await client.register.metrics());
+})
